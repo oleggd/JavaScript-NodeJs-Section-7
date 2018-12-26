caf37b2a5924794915f2d879ee6948429f2356e2
oleg <olegg.dov@gmail.com>
2018-12-26 21:44:08 +0200

    JavaScript  NodeJs [Section 7] - GET /todos/:id tests

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,22 @@ app.get('/todos', (req,res) => {
     });
 });
 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.send({todo});
+        } else {
+            res.status(404).send();
+        }
+    }, (err) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(3000, ()=>{
     console.log('Server started on port 3000');
