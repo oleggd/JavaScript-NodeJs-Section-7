723d0f2dbbdff310addd1f288448903141188bb0
oleg <olegg.dov@gmail.com>
2018-12-27 19:48:03 +0200

    JavaScript  NodeJs [Section 7] - POST todo by id

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,14 @@
+const {ObjectID} = require('mongodb');
+const { mongoose } = require('../server/db/mongoose');
+const { Todo } = require('../server/models/todo');
+const { User } = require('../server/models/user');
+
+// Todo.remove({})
+Todo.remove({}).then(result => {
+    console.log(result);
+});
+
+//Todo.findOneAndRemove
+Todo.findByIdAndRemove('aaaa').then((todo)=> {
+
+});
